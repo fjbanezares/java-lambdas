5ec1712d9e4651231212a0e53c2719fe0851aa17
fjbanezares <fjbanezares@gmail.com>
2020-04-19 03:50:34 +0100

    Best Practices simplification

--- src/main/java/com/lightbend/lambdas/MessageTransformers.java
+++ src/main/java/com/lightbend/lambdas/MessageTransformers.java
@@ -10,20 +10,13 @@ class MessageTransformers {
     static Function<Message,Instant> toTimestamp = Message::getTimestamp;
     static Function<Message,Message> toUpperCase = Message::upperCaseMessage;
 
-
     static <T> List<T> mapMessages(List<Message> messages, Function<Message,T> transformer) {
-        return messages
-                .stream()
-                .map(transformer)
-                .collect(Collectors.toList());
+        return messages.stream().map(transformer).collect(Collectors.toList());
     }
 
     private static <T> Function<Message, T> transformFunction(Function<Message,T> transformer) {
-        return new Function<Message, T>() {
-            @Override
-            public T apply(Message message) {
-                return transformer.apply(message);
+        return transformer::apply;
             }
-        };
-    }
+
+
 }
